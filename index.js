be60874aaeaa6796ee215d6a8e6513a45f9664a2
Matthew Leandres <m.leandres@hotmail.com>
2018-10-16 16:48:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
+
+// Dynamic Port Binding
+const PORT = process.env.PORT || 5000
+// will get PORT from environment variables OR 5000 (if not env var defined)
+app.listen(PORT)
