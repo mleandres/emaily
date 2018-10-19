d5bd95ea7935ba6ac3acc1ebe0725e62af8048e8
Matthew Leandres <m.leandres@hotmail.com>
2018-10-19 17:02:33 -0400

    added react client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h2>Hello</h2>
+        <a href='/auth/google'>Sign In With Google</a>
+      </div>
+    )
+  }
+}
+
+export default App
